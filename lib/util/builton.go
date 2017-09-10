4729a6cbe9f3474cfb4bf49b4145efbcd1189039
hallundbaek <tobias.h.petersen@gmail.com>
2017-09-10 23:25:16 +0200

    list -> slice in doc

--- lib/util/builton.go
+++ lib/util/builton.go
@@ -1,6 +1,6 @@
 package util
 
-// Shift returns the head and tail of a string list, given an empty list it will return ("", nil).
+// Shift returns the head and tail of a string slice, given an empty list it will return ("", nil).
 func Shift(slice []string) (head string, tail []string) {
   switch len(slice) {
   case 0:
