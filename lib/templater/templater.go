ac4fdcfa37f8d89c397e2999fdcd87a62196eb87
hallundbaek <tobias.h.petersen@gmail.com>
2017-10-17 13:26:37 +0200

    moved templater from frontend to own package

--- lib/templater/templater.go
+++ lib/templater/templater.go
@@ -6,12 +6,15 @@ import (
   "net/http"
   "text/template"
 
+  "github.com/oxtoacart/bpool"
+
   "diserve.didactia.org/lib/env"
 )
 
 // Templater struct to hold the loaded templates.
 type Templater struct {
   templates *template.Template
+  bufpool *bpool.BufferPool
   path string
 }
 
@@ -36,14 +39,36 @@ func (t *Templater) Render(w http.ResponseWriter, name string, data interface{})
   tmpl.Execute(w, data)
 }
 
+// RenderString renders template with name and data, and returns the rendered string.
+func (t *Templater) RenderString(name string, data interface{}) (string, error) {
+  var err error
+  if env.Vars.LOADTMPLONREQUEST {
+    t.templates, err = template.ParseGlob(t.path)
+    if err != nil {
+      return "", err
+    }
+  }
+  tmpl := t.templates.Lookup(name)
+  if tmpl == nil {
+    return "", fmt.Errorf("the template %s does not exist", name)
+  }
+  buf := t.bufpool.Get()
+  tmpl.Execute(buf, data)
+  str := buf.String()
+  t.bufpool.Put(buf)
+  return str, nil
+}
+
 // NewTemplater returns a Templater with parsed templates in the directory TMPLPATH.
 func NewTemplater(path string) *Templater {
   tmpls, err := template.ParseGlob(path)
   if err != nil {
     log.Fatal(err)
   }
+  bufpool := bpool.NewBufferPool(env.Vars.TMPLBPOOLSIZE)
   templater := &Templater{
     templates: tmpls,
+    bufpool: bufpool,
     path: path,
   }
   return templater
